85e750a6a97e52a07d1d9633bf1d5973e3518a4a
Kee <shikee.maloney@codeimmersives.com>
2023-08-26 21:42:27 -0400

    completed challenge one

--- FirstSteps/FloatingPointChallenge.java
+++ FirstSteps/FloatingPointChallenge.java
@@ -2,8 +2,10 @@ package FirstSteps;
 
 public class FloatingPointChallenge {
     public static void main(String[] args) {
-        
-        
+        // challenge is to assign numbers to variables and convert pounds to kilograms
+        int pounds = 5;
+        double kilogram = .45359237;
+        System.out.print(pounds * kilogram);
     }
     
 }
